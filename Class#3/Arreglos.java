027d738c482ba33ce53ac79a2c59f827d575e7e9
tecnico54 <115370228+tecnico54@users.noreply.github.com>
2025-02-28 00:52:22 +0000

    cambio

--- Class#3/Arreglos.java
+++ Class#3/Arreglos.java
@@ -1,5 +1,5 @@
 public class Arreglos{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int[] A = {3, 6, 5, 10, 20, 1, 4, 9};
         for (int i = 0; i < A.length; i++){
             System.out.println("A[" + i + "]=" + A[i]);
---
+++ Class#4/Vehiculo.java
@@ -0,0 +1,46 @@
+//Nombre (Instancia)
+public class Vehiculo{
+    //Atributo
+    private String marca;
+    private String color;
+    private String chasis;
+    private String potencia;
+    private double peso;
+    private double cilindraje;
+    //Constructo -> Inicializa el objeto
+    public Vehiculo(String marca, String color, String chasis, String potencia, double peso, double cilindraje){
+        this.marca = marca;
+        this.color = color;
+        this.chasis = chasis;
+        this.potencia = potencia;
+        this.peso = peso;
+        this.cilindraje = cilindraje;
+    }
+    //Metodos Getter / Setter
+    public String getMarca(){
+        return marca;
+    }
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    //Metodo para mostrar un objeto
+    public String toString(){
+        return "Vehiculo {Marca: " + marca + " Color: " + color + " Chasis " + chasis + " Potencia " + potencia + " Peso: " + peso + " Cilindraje: " + cilindraje + "}";
+    }
+    //Metodo de la clase
+    public void acelerar(){
+        System.out.println("Vehiculo Acelerando");
+    }
+    public void frenar(){
+        System.out.println("Vehiculo Frenando");
+    }
+    public void girar_Izquierda(){
+        System.out.println("Vehiculo girando a la izquierda");
+    }
+    public void girar_Derecha(){
+        System.out.println("Vehiculo girando a la derecha");
+    }
+    public void retroseder(){
+        System.out.println("Vehiculo retrosediendo");
+    }
+}
